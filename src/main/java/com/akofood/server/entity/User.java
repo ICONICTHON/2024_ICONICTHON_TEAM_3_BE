21f156f01e8a64fe3dc35771127144445a068e1e
khoon9 <kshun08@dongguk.edu>
2024-11-14 22:48:22 +0900

    Revert "Feat: api 사용한 결제 기능"
    This reverts commit 9f07f6ed11240ed84e5dfb5617e868d8c96cd095.

--- src/main/java/com/akofood/server/entity/User.java
+++ src/main/java/com/akofood/server/entity/User.java
@@ -2,7 +2,10 @@ package com.akofood.server.entity;
 
 import com.akofood.server.entity.global.BaseTimeEntity;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -14,21 +17,7 @@ public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String username;
-    private String email;
-    private String address;
 
     private String nickname;
 
-//    @Builder
-//    public User(String username) {
-//        this.username = username;
-//    } //유저 id만 받아오게 수정했음!!!
-
-    @Builder
-    public User(String username, String email, String address) {
-        this.username = username;
-        this.email = email;
-        this.address = address;
-    }
 }
--- src/main/java/com/akofood/server/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.akofood.server.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.akofood.server.entity.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}

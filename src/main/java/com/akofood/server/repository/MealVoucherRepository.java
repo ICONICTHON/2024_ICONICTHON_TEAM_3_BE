2d39bb8c67ff86f997ab46e3a030023587f4be96
khoon9 <kshun08@dongguk.edu>
2024-11-15 03:09:14 +0900

    Feat: 사용자 보유 식권 조회

--- src/main/java/com/akofood/server/repository/MealVoucherRepository.java
+++ src/main/java/com/akofood/server/repository/MealVoucherRepository.java
@@ -3,5 +3,8 @@ package com.akofood.server.repository;
 import com.akofood.server.entity.MealVoucher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MealVoucherRepository extends JpaRepository<MealVoucher, Long> {
+    List<MealVoucher> findByUserId(Long userId);
 }
